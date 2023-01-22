687832bd33fdf048510877a6b3bfc97defdf1117
KurnikovD <34297950+KurnikovD@users.noreply.github.com>
2023-01-22 19:40:36 +0300

    integer to roman algorithm

---
+++ src/leetcode/IntegerToRoman.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IntegerToRoman {
+    public String intToRoman(int num) {
+        StringBuilder result = new StringBuilder();
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                result.append(symbols[i]);
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        IntegerToRoman integerToRoman = new IntegerToRoman();
+        assertEquals("MCMXCVI", integerToRoman.intToRoman(1996));
+    }
+}
